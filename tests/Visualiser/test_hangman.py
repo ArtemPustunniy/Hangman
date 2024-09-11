c2a325374acd0ee25e5fc1a8ac127ec2e10e0fe0
ArtemPustunniy <artem1234562005@gmail.com>
2024-09-12 00:03:34 +0300

    fixed trubles with some test, which had emulated input again

--- tests/Visualiser/test_hangman.py
+++ tests/Visualiser/test_hangman.py
@@ -15,10 +15,10 @@ def static_hangman():
     return Static_Hangman(part_of_hangman=10, attempts=2)
 
 
-@pytest.fixture
-def dinamic_hangman(static_hangman):
-    steps_in_play = static_hangman.get_steps_in_play()
-    return Dinamic_Hangman(incorrect_guesses=2, steps_in_play=steps_in_play)
+# @pytest.fixture
+# def dinamic_hangman(static_hangman):
+#     steps_in_play = static_hangman.get_steps_in_play()
+#     return Dinamic_Hangman(incorrect_guesses=2, steps_in_play=steps_in_play)
 
 
 class TestHangman:
